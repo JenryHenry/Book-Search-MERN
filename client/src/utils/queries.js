9bd9b4037e97a174829b7518f6b0c76a2f98db80
Lizard <josephmhenry42@gmail.com>
2024-04-29 23:42:43 -0500

    replace most of API with graphQL queries/mutation client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const GET_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      savedBooks {
+        _id
+        authors
+        description
+        title
+      }
+    }
+  }
+`;
